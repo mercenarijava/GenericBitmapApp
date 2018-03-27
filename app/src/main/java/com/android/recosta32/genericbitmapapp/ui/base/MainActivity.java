0fa70424efa7e174c26bff4568468439022aee0e
luke <mercenarijava@gmail.com>
2018-03-27 13:31:06 +0200

    menu build commit

--- app/src/main/java/com/android/recosta32/genericbitmapapp/ui/base/MainActivity.java
+++ app/src/main/java/com/android/recosta32/genericbitmapapp/ui/base/MainActivity.java
@@ -6,16 +6,44 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.android.recosta32.genericbitmapapp.R;
 import com.android.recosta32.genericbitmapapp.ui.custom.ImageBoxComponent;
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingMenuLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FlowingDrawer drawer;
+    FlowingMenuLayout menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*DRAWER SETUP*/
+
+        drawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+        menu=findViewById(R.id.menulayout);
+        drawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+        drawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == ElasticDrawer.STATE_CLOSED) {
+                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(float openRatio, int offsetPixels) {
+                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+            }
+        });
+
+        /*END DRAWER SETUP*/
 
         ImageBoxComponent imageBoxComponent = (ImageBoxComponent) findViewById(R.id.test);
         imageBoxComponent.addImage(createImage(1024, 800, ContextCompat.getColor(getBaseContext(), R.color.black)), "#607D8B")
